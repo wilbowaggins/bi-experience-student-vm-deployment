6af8212c735ac15b898cbd0f8c9e61e41356912e
Mark Garner <mgarner@microsoft.com>
2015-10-30 18:23:35 +0000

    Initial Checkin

---
+++ deploy template.ps1
@@ -0,0 +1,13 @@
+﻿add-azureaccount
+
+Select-AzureSubscription -SubscriptionName "MTC Workshop"
+Get-AzureSubscription 
+
+Switch-AzureMode AzureResourceManager 
+
+$deployName="TestDeployment"
+$RGName="biexperiencevms"
+$locname="Central US"
+$templateURI="c:\biexperience\studentvms.json"
+New-AzureResourceGroup –Name $RGName –Location $locName
+New-AzureResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateUri $templateURI
