d23017c7f4cfcaee03dc1f0f59aca5ff63acfc39
Mark Garner <mgarner@microsoft.com>
2015-11-02 11:28:26 -0600

    updated pathing

--- deploy template.ps1
+++ deploy template.ps1
@@ -1,4 +1,4 @@
-﻿add-azureaccount
+add-azureaccount
 
 Select-AzureSubscription -SubscriptionName "MTC Workshop"
 Get-AzureSubscription 
@@ -8,6 +8,6 @@ Switch-AzureMode AzureResourceManager
 $deployName="TestDeployment"
 $RGName="biexperiencevms"
 $locname="Central US"
-$templateURI="c:\biexperience\studentvms.json"
+$templateURI=".\azuredeploy.json"
 New-AzureResourceGroup –Name $RGName –Location $locName
 New-AzureResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateUri $templateURI
