f1962985ed12ad2c3c39b8fa0bacce8f4784c6aa
wilbowaggins <wilbowaggins@users.noreply.github.com>
2016-07-14 11:43:34 -0500

    Fixed IE ESC powershell script

--- initializeStudentVM.ps1
+++ initializeStudentVM.ps1
@@ -1,6 +1,16 @@
-REG ADD "HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /v IsInstalled /t REG_DWORD /d 00000000 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /v IsInstalled /t REG_DWORD /d 00000000 /f
-
+Set-ItemProperty -Path “HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}” -Name isinstalled -Value 1
+Set-ItemProperty -Path “HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}” -Name isinstalled -Value 1
+Rundll32 iesetup.dll, IEHardenLMSettings,1,True
+Rundll32 iesetup.dll, IEHardenUser,1,True
+Rundll32 iesetup.dll, IEHardenAdmin,1,True
+If (Test-Path “HKCU:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”) 
+{
+Remove-Item -Path “HKCU:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+}
+If (Test-Path “HKCU:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”)
+{
+Remove-Item -Path “HKCU:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+}
 
 REG ADD "HKLM\Software\Microsoft\ServerManager" /v DoNotOpenServerManagerAtLogon /t REG_DWORD /d 00000001 /f
 
