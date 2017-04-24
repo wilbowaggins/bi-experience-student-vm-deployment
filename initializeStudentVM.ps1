1d9241a458552ca41696cb5003a8649c66568c49
wilbowaggins <wilbowaggins@users.noreply.github.com>
2017-04-24 16:10:52 -0500

    install chrome for PDF viewing
    why not Edge you ask? Well apparently you can't have that on a server OS :-|

--- initializeStudentVM.ps1
+++ initializeStudentVM.ps1
@@ -61,3 +61,5 @@ if ($install) {
    
     msiexec /i $msiPath ACCEPT_EULA=1 /passive /quiet /lv PBI_log.txt
 }
+
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install
